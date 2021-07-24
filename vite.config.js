6ab341e5987734bf3e71d7da7b19aa0f8d9d9d21
ianchen0119 <t106360734@ntut.edu.tw>
2021-07-24 17:12:25 +0800

    update the vite config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import Vue from '@vitejs/plugin-vue'
 import Icons from 'vite-plugin-icons'
 import { join } from 'path'
 
-// https://vitejs.dev/config/
+// https://cn.vitejs.dev/config/#resolve-conditions
+
 export default defineConfig({
+  base: './',
+  mode: 'production',
   resolve: {
     alias: {
       '@': `${join(__dirname, 'src')}`
